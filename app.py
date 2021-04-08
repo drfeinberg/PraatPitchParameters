be8d87ad0ed196868e1e71f332f9455f64ba23d8
David R. Feinberg <feinberg@mcmaster.ca>
2021-04-08 15:49:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,3 +32,6 @@ pitch_values[pitch_values==0] = np.nan
 y = pitch_values
 df = pd.DataFrame({"Frequency(Hz)":y})
 st.line_chart(df)
+
+
+st.text('Sound from: Livingstone SR, Russo FA (2018) The Ryerson Audio-Visual Database of Emotional Speech and Song (RAVDESS): A dynamic, multimodal set of facial and vocal expressions in North American English. PLoS ONE 13(5): e0196391. https://doi.org/10.1371/journal.pone.0196391.')
