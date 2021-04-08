a84285dcdf6af775d5d3ea0b087f4414719e76d5
David R. Feinberg <feinberg@mcmaster.ca>
2021-04-08 15:51:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,7 @@ df = pd.DataFrame({"Frequency(Hz)":y})
 st.line_chart(df)
 
 
-st.text('Sound from: Livingstone SR, Russo FA (2018) The Ryerson Audio-Visual Database of Emotional Speech and Song (RAVDESS): A dynamic, multimodal set of facial and vocal expressions in North American English. PLoS ONE 13(5): e0196391. https://doi.org/10.1371/journal.pone.0196391.')
+st.text("""Sound from: Livingstone SR, Russo FA (2018) 
+The Ryerson Audio-Visual Database of Emotional Speech and Song (RAVDESS):
+A dynamic, multimodal set of facial and vocal expressions in North American English. 
+PLoS ONE 13(5): e0196391. https://doi.org/10.1371/journal.pone.0196391.""")
