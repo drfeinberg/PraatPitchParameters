dae0f48c4b0bb1b958861f4515a308da6563161c
David R. Feinberg <feinberg@mcmaster.ca>
2021-05-11 11:36:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,33 +6,45 @@ import parselmouth
 import numpy as np
 import streamlit as st 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 st.markdown('# How Analysis Parameters Affect Pitch Measures in Praat')
 
 # Load sound into Praat
 sound = parselmouth.Sound("03-01-01-01-01-01-01.wav")
-waveform = pd.DataFrame({"Amplitude": sound.values[0].T})
-st.line_chart(waveform)
-
-
-# Load sound into streamlit
-
 
 audio_file = open('03-01-01-01-01-01-01.wav', 'rb')
 audio_bytes = audio_file.read()
 st.audio(audio_bytes, format='audio/wav')
 
-st.markdown("## Praat's Pitch Floor and Ceiling Settings")
-floor = st.slider('Pitch Floor', 50, 250, 75)
-ceiling = st.slider('Pitch Ceiling', 300, 700, 600)
+st.sidebar.markdown("## Praat's Pitch Floor and Ceiling Settings")
+floor = st.sidebar.slider('Pitch Floor', 50, 250, 75)
+ceiling = st.sidebar.slider('Pitch Ceiling', 300, 700, 600)
+kill_octave_jumps = st.sidebar.checkbox("Kill Octave Jumps")
+
+
 pitch = sound.to_pitch(pitch_floor=floor, pitch_ceiling=ceiling)
+if kill_octave_jumps:
+    parselmouth.praat.call(pitch, "Kill octave jumps")
+
 x = pitch.xs()
 pitch_values = pitch.selected_array['frequency']
 pitch_values[pitch_values==0] = np.nan
 y = pitch_values
-df = pd.DataFrame({"Frequency(Hz)":y})
-st.line_chart(df)
+df = pd.DataFrame({"Time (s)": x,
+                  "Frequency (Hz)":y})
 
+st.markdown(f"# Mean Pitch: {round(df['Frequency (Hz)'].mean(), 3)}")
+fig, ax = plt.subplots()
+ax.plot(df['Time (s)'], df['Frequency (Hz)'], marker='o')
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Frequency (Hz)')
+ax.grid(True)
+st.pyplot(fig)
+
+st.markdown(f"# Data")
+st.markdown(f"## NaN's filtered out")
+st.table(df.dropna())
 
 st.text("""Sound from: Livingstone SR, Russo FA (2018) 
 The Ryerson Audio-Visual Database of Emotional Speech and Song (RAVDESS):
